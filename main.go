81b0d6427c3cfd3e7bd5a11cdf5d8bc82a69a712
root <aslanovnicat@gmail.com>
2021-03-21 18:01:16 +0400

    initial

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for len(numbers) > 0 {
+
+		numbersize := len(numbers)
+		random := rand.Intn(numbersize)
+		//random number print
+		fmt.Println(numbers[random])
+		//Delete previously printed element from list
+		numbers = append(numbers[:random], numbers[random+1:]...)
+
+	}
+
+}
