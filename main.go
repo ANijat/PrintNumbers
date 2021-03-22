8e271b48faef2ea8f250367d1cd886adfe19ab34
Nijat Aslanov <50009815+ANijat@users.noreply.github.com>
2021-03-22 23:01:55 +0400

    Update main.go
    var

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for len(numbers) > 0 {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for len(nums) > 0 {
 
-		numbersize := len(numbers)
-		random := rand.Intn(numbersize)
+		numsize := len(nums)
+		r := rand.Intn(numsize)
 		//random number print.
-		fmt.Println(numbers[random])
+		fmt.Println(nums[r])
 		//Delete previously printed element from list.
-		numbers = append(numbers[:random], numbers[random+1:]...)
+		nums = append(nums[:r], nums[r+1:]...)
 
 	}
 
